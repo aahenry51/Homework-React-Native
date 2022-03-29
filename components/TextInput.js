29147b5f48efbd612a8900d0d75eaffbe351749a
Alphi Henry <aahenry51@gmail.com>
2022-03-28 22:58:58 -0400

    Text Input Example

---
+++ components/TextInput.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import {TextInput, View, Text} from 'react-native';
+import {styles} from './TextInput_styles';
+
+export const TextInputComponent = () => {
+  const [text, onChangeText] = useState('Hello', '');
+
+  return (
+    <View style={styles.container}>
+      <Text>{text}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </View>
+  );
+};
---
+++ components/TextInput_styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  container: {
+    height: 300,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
